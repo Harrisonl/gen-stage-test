36fe5e0b0e39d5e0751c76c937b638f6a5317d05
harrisonl <harrisonlucas@live.com>
2016-12-09 13:59:50 +1100

    udpated to use ets

--- lib/gs_example.ex
+++ lib/gs_example.ex
@@ -2,37 +2,15 @@ alias Experimental.GenStage
 
 defmodule Go do
   def go do
-    {:ok, producer} = GenStage.from_enumerable(File.stream!("nasa.log"))
+    :ets.new(:logs, [:named_table, :public])
+
+    {:ok, producer} = GenStage.from_enumerable(File.stream!("nasa.log"), name: __MODULE__)
     {:ok, consumer1} = GsConsumer.start_link("1")
-    {:ok, consumer2} = GsConsumer.start_link("2")
-    {:ok, consumer3} = GsConsumer.start_link("3")
-    {:ok, consumer4} = GsConsumer.start_link("4")
-
-    GenStage.sync_subscribe(consumer1, to: producer)
-    GenStage.sync_subscribe(consumer2, to: producer)
-    GenStage.sync_subscribe(consumer3, to: producer)
-    GenStage.sync_subscribe(consumer4, to: producer)
+    {:ok, consumer1} = GsConsumer.start_link("1")
+    {:ok, consumer1} = GsConsumer.start_link("1")
+    {:ok, consumer1} = GsConsumer.start_link("1")
   end
 
-end
-
-defmodule GsProducer do
-  use GenStage
-
-  def start_link(file) do
-    IO.inspect file
-    GenStage.start_link(__MODULE__, file, name: __MODULE__)
-  end
-
-  def init(file) do
-    stream = File.stream! file
-    {:producer, stream}
-  end
-
-  def handle_demand(demand, stream) when demand > 0 do
-    lines = stream |> Stream.take(demand) |> Enum.to_list
-    {:noreply, lines, Stream.drop(stream, demand)}
-  end
 end
 
 defmodule GsConsumer do
@@ -43,20 +21,21 @@ defmodule GsConsumer do
   end
 
   def init([num]) do
-    {:consumer, num}
+    {:consumer, num, subscribe_to: [Go]}
   end
 
   def handle_events(lines, _from, num) do
-    File.open("results-#{num}.log", [:append], fn(file) ->
       lines
-      |> Enum.map(fn(line) ->
-        << head::binary-size(1), rest::binary>> = line
-        head
+      |> Enum.each(fn(line) ->
+        reg = ~r/GET \/[a-z]{0,10}\b([-a-zA-Z0-9@:%_\+.~#?&\/]*)/
+        case Regex.run(reg, line) do
+          nil -> ""
+          [h | tail] -> 
+            x = String.to_atom(h) 
+            :ets.update_counter(:logs, x, 1, {x, 1})
+        end
       end)
-      |> Enum.map(fn(x) ->
-        IO.binwrite(file, x)
-      end)
-    end)
-    {:noreply, [], nil}
+    {:noreply, [], num}
   end
 end
+
